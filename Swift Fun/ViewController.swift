ced2ec09399a9ad7c492dfd78b698663ec1b0903
Unknown <michaelwaynejnr@icloud.com>
2018-01-13 13:17:30 -0500

    Now you can ADD or SUBTRACT!

--- Swift Fun/ViewController.swift
+++ Swift Fun/ViewController.swift
@@ -9,34 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var buttonCount = 0
+    
     @IBOutlet weak var myLabel: UILabel!
     
+    @IBOutlet weak var topTextField: UITextField!
+    
+    @IBOutlet weak var bottomTextField: UITextField!
+    
+    @IBOutlet weak var additionSwitch: UISwitch!
     
     @IBAction func buttonTapped(_ sender: Any) {
-        buttonCount += 1
-      
-        if buttonCount >= 10{
-            view.backgroundColor = UIColor.red
-            myLabel.text = "You hit more than 10 times!"
+        let addition = additionSwitch.isOn
+        if addition{
+            let sum = Double(topTextField.text!)! + Double(bottomTextField.text!)!
+            myLabel.text = "\(topTextField.text!) + \(bottomTextField.text!) = \(sum)"
         }
-        if buttonCount >= 15{
-            view.backgroundColor = UIColor.green
-            myLabel.text = "You hit more than 15 times!"
+        else{
+            let sum = Double(topTextField.text!)! - Double(bottomTextField.text!)!
+            myLabel.text = "\(topTextField.text!) - \(bottomTextField.text!) = \(sum)"
         }
-    }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
